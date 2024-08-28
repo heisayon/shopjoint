d8596db1e392d40c29e30222e521536c37bffbe6
Zion <heisayon@gmail.com>
2024-08-28 23:48:49 +0100

    cart styles

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,8 +3,8 @@ import CartProduct from "../components/CartProduct";
 function Cart() {
   const cartItems = useSelector((state) => state.cartSlice.cart);
   return (
-    <section className="flex gap-6 max-sm:flex-col">
-      <div className="flex justify-center items-center flex-wrap gap-5 max-w-[500px] border-2 overflow-y-scroll h-[90vh] max-sm:h-[50vh]">
+    <section className="flex gap-6 max-sm:flex-col justify-center items-center">
+      <div className="flex justify-center items-center flex-wrap gap-5 max-w-[500px] overflow-y-scroll h-[90vh] max-sm:h-[60vh] pb-3">
         {cartItems.map((product, i) => (
           <CartProduct
             name={product.title}
@@ -17,9 +17,15 @@ function Cart() {
           />
         ))}
       </div>
-      <div className="border flex-1 max-sm:fixed max-sm:bottom-0 max-sm:left-0 max-sm:right-0 max-sm:h-[40%] max-sm:bg-white">
-        <h1>Check Out</h1>
-      </div>
+      {cartItems.length > 0 && (
+        <div className="flex-1 max-sm:fixed max-sm:bottom-0 max-sm:left-0 max-sm:right-0 max-sm:h-[30vh] max-sm:bg-white">
+          <h1 className="">Check Out</h1>
+          <div>
+            <h1>Shipping: </h1>
+            <h1>Total: </h1>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
