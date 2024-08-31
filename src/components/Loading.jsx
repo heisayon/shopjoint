7ac647d0296ace77768eb7106ac1d3e1dce1030a
heisayon <heisayon@gmail.com>
2024-08-31 04:23:23 +0100

    updated

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import { Spinner } from "flowbite-react"
 
 function Loading() {
   return (
-    <div className="flex justify-center items-center h-[90vh]">
+    <div className="flex justify-center items-center h-[80vh]">
     <Spinner aria-label="Loading State" size="xl" />
     </div>
   )
