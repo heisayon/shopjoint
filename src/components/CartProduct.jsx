422de60453e307eba514283527afe432c09e76f7
heisayon <heisayon@gmail.com>
2024-08-31 16:46:46 +0100

    reset

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -1,21 +1,11 @@
 import { Button, Card } from "flowbite-react";
 import Ratings from "./Ratings";
 import PropTypes from "prop-types";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { removeFromCart } from "../features/cartSlice";
-import {
-  increaseCount,
-  decreaseCount,
-  totalPrice,
-} from "../features/cartSlice";
-import { useEffect } from "react";
 function CartProduct({ image, rating, name, availability, price, data }) {
-  const count = useSelector((state) => state.cartSlice.numberOfProducts);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(totalPrice());
-  }, [count]);
+;
   return (
     <Card className=" max-w-xs" imgAlt={name} imgSrc={image}>
       <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
@@ -26,23 +16,6 @@ function CartProduct({ image, rating, name, availability, price, data }) {
         <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
           {availability}
         </span>
-      </div>
-      <div className="flex justify-center">
-        <div className="flex border-2 px-6 py-3 items-center justify-between rounded-lg w-[150px]">
-          <button
-            onClick={() => dispatch(increaseCount())}
-            className=" bg-gray-500 px-3 text-lg font-bold"
-          >
-            +
-          </button>
-          <h1 className="font-semibold">{count}</h1>
-          <button
-            onClick={() => dispatch(decreaseCount())}
-            className=" bg-gray-500 px-3 text-lg font-bold"
-          >
-            -
-          </button>
-        </div>
       </div>
       <div className="flex items-center justify-between">
         <span className="text-3xl font-bold text-gray-900 dark:text-white">
--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -5,7 +5,6 @@ const initialState = {
   total: 0,
   productPrice: 0,
   shippingFee: 0.5,
-  numberOfProducts: 1,
 };
 
 export const cartSlice = createSlice({
@@ -27,24 +26,12 @@ export const cartSlice = createSlice({
       let allPrice = 0;
       let shipment = state.shippingFee * state.cart.length;
       state.cart.forEach((products) => (allPrice += products.cartItems.price));
-      state.productPrice = allPrice * state.numberOfProducts;
-      state.total = state.productPrice + shipment;
-    },
-    increaseCount: (state) => {
-      state.numberOfProducts += 1;
-    },
-    decreaseCount: (state) => {
-      state.numberOfProducts -= 1;
+      state.total = allPrice + shipment;
+      state.productPrice = allPrice;
     },
   },
 });
 
-export const {
-  addToCart,
-  removeFromCart,
-  totalPrice,
-  increaseCount,
-  decreaseCount,
-} = cartSlice.actions;
+export const { addToCart, removeFromCart, totalPrice } = cartSlice.actions;
 
 export default cartSlice.reducer;
